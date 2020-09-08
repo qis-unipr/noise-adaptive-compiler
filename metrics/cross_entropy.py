63e596280b51cec9f5a9196f93c82fcaea60ae94
Davide Ferrari <davidefrr@gmail.com>
2020-09-08 10:10:13 +0200

    Fix bugs in metrics

--- metrics/cross_entropy.py
+++ metrics/cross_entropy.py
@@ -1,8 +1,9 @@
 from functools import reduce
-from math import log
+from math import log, exp
 
 
 def cross_entropy(real_counts, ideal_probs):
     total_real = reduce(lambda a, b: a + b, real_counts.values())
-    ce = reduce(lambda a, b: a+b, list(map(lambda x: real_counts[x]*log(1/ideal_probs[x], 2), real_counts)))
+    n_qubits = len(list(ideal_probs.keys())[0])
+    ce = reduce(lambda a, b: a+b, list(map(lambda x: log(1/ideal_probs[x], 2) if x in ideal_probs else log(1/exp(-n_qubits), 2), real_counts)))
     return ce / total_real
--- metrics/heavy_output_generation.py
+++ metrics/heavy_output_generation.py
@@ -5,5 +5,5 @@ from statistics import median
 def hog(real_counts, ideal_probs):
     total_real = reduce(lambda a, b: a + b, real_counts.values())
     med = median(ideal_probs.values())
-    hog = reduce(lambda a,b: a+b, list(map(lambda x: real_counts[x] if real_counts[x]/total_real > med else 0, real_counts)))
+    hog = reduce(lambda a,b: a+b, list(map(lambda x: real_counts[x] if x in ideal_probs and ideal_probs[x] > med else 0, real_counts)))
     return hog / total_real
