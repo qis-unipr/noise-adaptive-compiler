3dc7af61275984ba8ca76fce79307626bc42308b
Davide Ferrari <davidefrr@gmail.com>
2020-09-09 16:41:45 +0200

    remove files from merge

--- metrics/__init__.py
+++
@@ -1,3 +0,0 @@
-from .heavy_output_generation import hog
-from .cross_entropy import cross_entropy
-from .l1_norm import l1_norm
--- metrics/cross_entropy.py
+++
@@ -1,9 +0,0 @@
-from functools import reduce
-from math import log, exp
-
-
-def cross_entropy(real_counts, ideal_probs):
-    total_real = reduce(lambda a, b: a + b, real_counts.values())
-    n_qubits = len(list(ideal_probs.keys())[0])
-    ce = reduce(lambda a, b: a+b, list(map(lambda x: log(1/ideal_probs[x], 2) if x in ideal_probs else log(1/exp(-n_qubits), 2), real_counts)))
-    return ce / total_real
